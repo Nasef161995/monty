c9b0c4d8ec9e56538d3c6d03ad112c1d02105a5f
Nasef161995~ <mnasef905@gmail.com>
2023-09-21 11:18:49 +0300

    fff

--- open_file.c
+++ open_file.c
@@ -7,41 +7,41 @@
  */
 void run(stack_t **stack, char *filename)
 {
-    char line[100];
-    char *line_copy, *_opcode;
-    int i, check;
-    unsigned int count = 0;
-    FILE *file;
-    instruction_t operators_code[] = {{"push", op_push},
-                                      {"pall", op_pall},
-                                      {"pint", op_pint},
-                                      {"pop", op_pop},
-                                      {"swap", op_swap},
-                                      {"add", op_add},
-                                      {"nop", op_nop}};
-    file = fopen(filename, "r");
-    if (file == NULL)
-    {
-        printf("Error: Can't open file %s\n", filename);
-        error_exit(stack);
-    }
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        count++;
-        line_copy = strcat(line, "\0");
-        _opcode = strtok(line_copy, " ,\n");
-        if (_opcode == NULL)
-            continue;
-        i = find_opcode(_opcode, operators_code);
-        if (i >= 0)
-            operators_code[i].f(stack, count);
-        else
-        {
-            printf("L%d: unknown instruction %s\n", count, line_copy);
-            error_exit(stack);
-        }
-    }
-    check = fclose(file);
-    if (check == -1)
-        exit(-1);
+char line[100];
+char *line_copy, *_opcode;
+int i, check;
+unsigned int count = 0;
+FILE *file;
+instruction_t operators_code[] = {{"push", op_push},
+{"pall", op_pall},
+{"pint", op_pint},
+{"pop", op_pop},
+{"swap", op_swap},
+{"add", op_add},
+{"nop", op_nop}};
+file = fopen(filename, "r");
+if (file == NULL)
+{
+printf("Error: Can't open file %s\n", filename);
+error_exit(stack);
+}
+while (fgets(line, sizeof(line), file) != NULL)
+{
+count++;
+line_copy = strcat(line, "\0");
+_opcode = strtok(line_copy, " ,\n");
+if (_opcode == NULL)
+continue;
+i = find_opcode(_opcode, operators_code);
+if (i >= 0)
+operators_code[i].f(stack, count);
+else
+{
+printf("L%d: unknown instruction %s\n", count, line_copy);
+error_exit(stack);
+}
+}
+check = fclose(file);
+if (check == -1)
+exit(-1);
 }
