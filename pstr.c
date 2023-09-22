12ce7d6129d7ccbafa318cdedac78cbccfc94afc
Nasef161995~ <mnasef905@gmail.com>
2023-09-22 05:02:19 +0300

    ddddd

--- pstr.c
+++ pstr.c
@@ -9,18 +9,14 @@ void op_pstr(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
 stack_t *temp;
 
-if ((*stack) == NULL)
-{
-printf("\n");
-}
 
 temp = *stack;
 while (temp != NULL)
 {
-if (temp->n == 0)
+if (temp->n <= 0 || temp->n > 127)
+{
 break;
-if (!((*stack)->n >= 0 && (*stack)->n <= 127))
-break;
+}
 printf("%c", temp->n);
 temp = temp->next;
 }
